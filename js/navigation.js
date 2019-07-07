023f7214d8ef56eefaafd2b9bea7c56f53196db4
vincent <670700452@qq.com>
2019-07-07 11:16:12 +0800

    添加导航栏功能

---
+++ js/navigation.js
@@ -0,0 +1,34 @@
+setTimeout(function () {
+  siteWelcome.classList.remove('active')
+}, 400)
+
+window.onscroll = function () {
+  if (window.scrollY > 0) {
+    topNavBar.classList.add('sticky')
+  } else {
+    topNavBar.classList.remove('sticky')
+  }
+}
+
+
+let liTags = document.querySelectorAll('nav.menu > ul > li')
+for (let i = 0; i < liTags.length; i++) {
+  liTags[i].onmouseenter = function (x) {
+    x.currentTarget.classList.add('active')
+  }
+  liTags[i].onmouseleave = function (x) {
+    x.currentTarget.classList.remove('active')
+  }
+}
+
+let aTags = document.querySelectorAll('nav.menu > ul > li > a')
+for (let i = 0; i < aTags.length; i++) {
+  aTags[i].onclick = function (x) {
+    x.preventDefault()
+    let a = x.currentTarget
+    let href = a.getAttribute('href')
+    let element = document.querySelector(href)
+    let top = element.offsetTop
+    window.scrollTo(0, top - 80)
+  }
+}
