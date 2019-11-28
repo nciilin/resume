e421b13cb462da5ac8c38c9072d69e74ba6a53df
vincentcheng <670700452@qq.com>
2019-11-28 20:34:03 +0800

    edit resume

---
+++ js/btn.js
@@ -0,0 +1,22 @@
+var btnTraval = document.querySelector('#linktraval')
+var btnresume = document.querySelector('#linkresume')
+var btncanvas = document.querySelector('#linkcanvas')
+var btnPikachu = document.querySelector('#linkPikachu')
+
+btnTraval.onclick = function() {
+  window.open('http://bloglive.top/Traval-demo/Traval/dist')
+}
+
+btnresume.onclick = function() {
+  window.open('http://bloglive.top/resume')
+}
+
+btncanvas.onclick = function() {
+  window.open('http://bloglive.top/canvas-demo/')
+}
+
+btnPikachu.onclick = function() {
+  window.open('https://bloglive.top/Pikachu-demo/')
+}
+
+
